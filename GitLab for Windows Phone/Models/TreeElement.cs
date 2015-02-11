8d894a6537cc4c5dfcb54771de4a4c3d3af30cc5
Mickael GOETZ <contact@mickael-goetz.com>
2015-02-11 17:54:02 +0100

    WIP TreeElement

---
+++ GitLab for Windows Phone/Models/TreeElement.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace GitLab_for_Windows_Phone.Models
+{
+    [DataContract]
+    public class TreeElement
+    {
+        [DataContract]
+        public enum TreeElementTypes
+        {
+            [EnumMember]
+            Unknow,
+            [EnumMember(Value = "tree")]
+            Folder,
+            [EnumMember(Value = "blob")]
+            File
+        }
+
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "type")]
+        public TreeElementTypes Type { get; set; }
+
+        [DataMember(Name = "mode")]
+        public int Mode { get; set; }
+    }
+}
